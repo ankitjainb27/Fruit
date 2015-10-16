dc8d9e3fffc88e86c61829aa7db35efb2ee7a68c
Rohit Arya <arya.rohit07@gmail.com>
2015-10-16 15:48:18 +0530

    init

--- app/src/main/java/com/housing/typeracer/ConnectionUtils.java
+++ app/src/main/java/com/housing/typeracer/ConnectionUtils.java
@@ -4,6 +4,16 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AppIdentifier;
+import com.google.android.gms.nearby.connection.AppMetadata;
+import com.google.android.gms.nearby.connection.Connections;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gayathri_nair on 16/10/15.
  */
@@ -22,5 +32,40 @@ public class ConnectionUtils {
             }
         }
         return false;
+    }
+
+    public static void startAdvertising(GoogleApiClient mGoogleApiClient, Connections.ConnectionRequestListener listener) {
+        if (!isConnectedToNetwork()) {
+            // Implement logic when device is not connected to a network
+            MainApplication.showToast(R.string.not_connected_to_network);
+            return;
+        }
+
+        // Identify that this device is the host
+        MainApplication.mIsHost = true;
+
+        // Advertising with an AppIdentifer lets other devices on the
+        // network discover this application and prompt the user to
+        // install the application.
+        List<AppIdentifier> appIdentifierList = new ArrayList<>();
+        appIdentifierList.add(new AppIdentifier(MainApplication.getContext().getPackageName()));
+        AppMetadata appMetadata = new AppMetadata(appIdentifierList);
+
+        // The advertising timeout is set to run indefinitely
+        // Positive values represent timeout in milliseconds
+        long NO_TIMEOUT = 0L;
+
+        String name = null;
+        Nearby.Connections.startAdvertising(mGoogleApiClient, name, appMetadata, NO_TIMEOUT, listener).setResultCallback(new ResultCallback<Connections.StartAdvertisingResult>() {
+            @Override
+            public void onResult(Connections.StartAdvertisingResult result) {
+                if (result.getStatus().isSuccess()) {
+                    MainApplication.showToast(R.string.advertising_now);
+                } else {
+                    int statusCode = result.getStatus().getStatusCode();
+                    MainApplication.showToast(R.string.something_went_wrong);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/housing/typeracer/MainApplication.java
+++ app/src/main/java/com/housing/typeracer/MainApplication.java
@@ -2,12 +2,14 @@ package com.housing.typeracer;
 
 import android.app.Application;
 import android.content.Context;
+import android.widget.Toast;
 
 /**
  * Created by gayathri_nair on 16/10/15.
  */
 public class MainApplication extends Application {
     private static Context context;
+    public static boolean mIsHost = false;
 
     @Override
     public void onCreate() {
@@ -19,4 +21,13 @@ public class MainApplication extends Application {
     public static Context getContext() {
         return context;
     }
+
+    public static void showToast(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showToast(int id) {
+        Toast.makeText(context, context.getResources().getString(id), Toast.LENGTH_LONG).show();
+    }
+
 }
